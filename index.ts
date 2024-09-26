8aada937bcddd89545fd6c0dd8e6a036263d6fe4
kmankan <malink027@gmail.com>
2024-09-26 15:10:00 -0400

    frontend-backend-connected

--- index.ts
+++ index.ts
@@ -1,15 +1,38 @@
 const express = require('express');
-const app = express();
+const cors = require('cors')
 const port = 3000;
-const frontend = 'http://localhost:5173/'
 
-app.get('/', (req,res) => {
-  res.send("Hello World")
+const app = express();
+app.use(cors());
+
+
+
+const dummyData:string[] = ['some data lives here', 'some second bit of data lives here']
+let name:string[] = ['first'];
+
+//Middleware
+app.use(express.json())
+
+app.get('/names', (req,res) => {
+  res.send(name)
+  console.log('testing that logging works')
+  console.log(name)
 });
 
-app.get('/app', (req,res) => {
-  res.send(frontend)
+app.get('/first', (req,res) => {
+  res.send(dummyData[0])
 });
+
+app.get('/second', (req,res) => {
+  res.send(dummyData[1])
+});
+
+app.post("/submit", (req,res) => {
+  const info = req.body;
+  console.log(req.body)
+  name.push(info.name);
+  res.send('Data recieved and stored');
+})
 
 app.listen(port, () => {
   console.log(`we are listening on port ${port}`)
